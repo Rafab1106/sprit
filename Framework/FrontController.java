ff85a46914e846ee03597b121c5e0d7494c8858d
Rafab1106 <fabioandry96@gmail.com>
2024-06-07 09:05:46 +0300

    sprit5 finish

--- Framework/FrontController.java
+++ Framework/FrontController.java
@@ -20,28 +20,37 @@ public class FrontController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        urlMappings = new HashMap<>();
         try {
-            if (!isScan) {
-                ServletContext context = getServletContext();
-                String chemin = context.getInitParameter("chemin");
-                List<String> controllers = scan(chemin); // Utilisation de la méthode scan mise à jour
-                for (String controller : controllers) {
-                    Class<?> clazz = Class.forName(controller);
-                    Method[] methods = clazz.getDeclaredMethods();
-                    for (Method method : methods) {
-                        if (method.isAnnotationPresent(GET.class)) {
-                            GET annotation = method.getAnnotation(GET.class);
-                            String url = annotation.value();
-                            urlMappings.put(url, new Mapping(clazz.getName(), method.getName()));
-                        }
-                    }
-                }    
-            }
-            
+            ServletContext context = getServletContext();
+            String chemin = context.getInitParameter("chemin");
+            this.setMapping(chemin);
         } catch (Exception e) {
             throw new ServletException("Erreur lors de l'initialisation du FrontController", e);
         }
+    }
+    private void setMapping(String chemin)throws Exception{
+        urlMappings = new HashMap<>();
+        List<String> controllers = scan(chemin); // Utilisation de la méthode scan mise à jour
+            for (String controller : controllers) {
+                Class<?> clazz = Class.forName(controller);
+                Method[] methods = clazz.getDeclaredMethods();
+                boolean hasGetMethod = false; // Flag pour vérifier les méthodes @GET
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(GET.class)) {
+                        hasGetMethod = true;
+                        GET annotation = method.getAnnotation(GET.class);
+                        String url = annotation.value();
+                        if (urlMappings.containsKey(url)) {
+                            throw new Exception("Duplicate url ["+ url +"] dans "+ clazz.getName() + " et "+ urlMappings.get(url).getClassName());
+                        }
+                        urlMappings.put(url, new Mapping(clazz.getName(), method.getName()));
+                    }
+                }    
+                
+                if (!hasGetMethod) {
+                    throw new Exception("La classe " + clazz.getName() + " n'a aucune méthode annotée avec @GET.");
+                }
+            }
     }
     private List<String> scan(String chemin) throws Exception {
         List<String> liste = new ArrayList<>();
@@ -74,64 +83,67 @@ public class FrontController extends HttpServlet {
         this.isScan = true;
         return liste;
     }
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException,Exception {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        try {
-            String requestUrl = request.getRequestURI().substring(request.getContextPath().length());
-            Mapping mapping = urlMappings.get(requestUrl);
-            
-            if (mapping != null) {
-                Class<?> class1 = Class.forName(mapping.getClassName());
-                String methodName = mapping.getMethodName();
-                
-                Method method = class1.getMethod(methodName);
-                Object instance = class1.getDeclaredConstructor().newInstance();
-                
-                Object result = new Object();
-                if (method.getReturnType() == String.class) {
-
-                    result = (String) method.invoke(instance);    
-                    out.println("<h1>URL: " + requestUrl + "</h1>");
-                    out.println("<p>Class: " + mapping.getClassName() + "</p>");
-                    out.println("<p>Method: " + mapping.getMethodName() + "</p>");
-                    out.println("<p>Resultat: " + result + "</p>");
-
-                } else if (method.getReturnType() == ModelView.class) {
-                    ModelView modelViewResult = (ModelView) method.invoke(instance);
-                    String url = modelViewResult.getUrl();
-                    HashMap<String, Object> data = modelViewResult.getMap();
-                    for (String key : data.keySet()) {
-                        request.setAttribute(key, data.get(key));
-                    }
-                    // Dispatcher les données vers l'URL
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-                    dispatcher.forward(request, response);
-                    System.out.println("the return is ModelandView");
-
-                } else {
-                    out.println("<p>Resultat: non reconue</p>");
-                }
-            } else {
-                out.println("<h1>No method associated with URL: " + requestUrl + "</h1>");
-            }
+        String requestUrl = request.getRequestURI().substring(request.getContextPath().length());
+        Mapping mapping = urlMappings.get(requestUrl);
         
-        } catch(Exception e) {
-            e.printStackTrace();
-        } 
-        finally {
-            out.close();
+        if (mapping != null) {
+            Class<?> class1 = Class.forName(mapping.getClassName());
+            String methodName = mapping.getMethodName();
+            
+            Method method = class1.getMethod(methodName);
+            Object instance = class1.getDeclaredConstructor().newInstance();
+            
+            Object result = new Object();
+            if (method.getReturnType() == String.class) {
+
+                result = (String) method.invoke(instance);    
+                out.println("<h1>URL: " + requestUrl + "</h1>");
+                out.println("<p>Class: " + mapping.getClassName() + "</p>");
+                out.println("<p>Method: " + mapping.getMethodName() + "</p>");
+                out.println("<p>Resultat: " + result + "</p>");
+
+            } else if (method.getReturnType() == ModelView.class) {
+                ModelView modelViewResult = (ModelView) method.invoke(instance);
+                String url = modelViewResult.getUrl();
+                HashMap<String, Object> data = modelViewResult.getMap();
+                for (String key : data.keySet()) {
+                    request.setAttribute(key, data.get(key));
+                }
+                // Dispatcher les données vers l'URL
+                RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+                dispatcher.forward(request, response);
+                System.out.println("the return is ModelandView");
+
+            } else {
+                throw new Exception("le type de retour est non reconue");
+            }
+        } else {
+            out.println("<h1>No method associated with URL: " + requestUrl + "</h1>");
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);    
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);    
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
     }
 
     
